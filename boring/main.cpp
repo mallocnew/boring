96bb355a6a3b265519fd8174da3c14ef456aa9dc
boring <none>
2022-04-17 22:37:16 +0800

    Main cplusplus

---
+++ boring/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+  std::cout << __cplusplus << std::endl;
+  std::cout << "Hello Cpp ISO" << std::endl;
+  return 0;
+}
